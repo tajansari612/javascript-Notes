2ebb3afa64efc9d0b1676c7e3052865ed8137e7a
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-04 11:39:17 +0530

    added new project 01_unlimitedColors

--- Programs/09_projects/01_unlimitedColors/01_app.js
+++ Programs/09_projects/01_unlimitedColors/01_app.js
@@ -1,2 +1,42 @@
-let hello = "hello";
-console.log(hello);
+
+//Selectors
+const body = document.querySelector('.body');
+const start = document.querySelector('#startBtn');
+const stop = document.querySelector('#stopBtn');
+
+//Functions
+function randomColor() {
+    const hex = "0123456789ABCDEF";
+    let color= '#';
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+let intervalId; 
+function startChangingColor(){
+    //If we not use below if condition then as many time you click it is called
+    // it called and stoping it not work
+    if(!intervalId){
+        intervalId = setInterval(() =>{
+            body.style.backgroundColor = randomColor();
+            console.log(randomColor());
+        },1000);
+    }
+    
+}
+
+function stopChangingColor() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+//Processing
+start.addEventListener('click',(e) =>{
+    console.log("Color changing started!");
+    startChangingColor();
+})
+
+stop.addEventListener('click',(e) =>{
+    stopChangingColor();
+})
